a28e12f1526ec039e2e7728daab7fecadcbfea1b
Caolan McMahon <caolan@caolanmcmahon.com>
2010-04-11 20:49:27 +0100

    added read function to metadata module

--- test/test-metadata.js
+++ test/test-metadata.js
@@ -48,3 +48,47 @@ exports.testInvalid = function(test){
     }
     test.done();
 };
+
+exports.testRead = function(test){
+    test.expect(2);
+    var parse_copy = metadata.parse;
+    metadata.parse = function(data){
+        test.equals(data, 'some data\n');
+        return 'metadata';
+    };
+    var filename = __dirname + '/fixtures/metadata_file';
+    metadata.read(filename, function(err, data){
+        test.same(data, 'metadata');
+        metadata.parse = parse_copy;
+        test.done();
+    });
+};
+
+exports.testReadNoFile = function(test){
+    test.expect(1);
+    var parse_copy = metadata.parse;
+    metadata.parse = function(data){
+        test.ok(false, 'should not be called');
+    };
+    var filename = __dirname + '/fixtures/doesnotexist';
+    metadata.read(filename, function(err, data){
+        test.ok(err instanceof Error, 'read returns fs error');
+        metadata.parse = parse_copy;
+        test.done();
+    });
+};
+
+exports.testReadParseError = function(test){
+    test.expect(1);
+    var parse_copy = metadata.parse;
+    var e = new Error('test');
+    metadata.parse = function(data){
+        throw e;
+    };
+    var filename = __dirname + '/fixtures/metadata_file';
+    metadata.read(filename, function(err, data){
+        test.equals(err, e, 'read returns parse error');
+        metadata.parse = parse_copy;
+        test.done();
+    });
+};
