158fe6e904a12c2c1aa4cfe0b61aca7e1728fba3
Caolan McMahon <caolan@caolanmcmahon.com>
2010-04-12 21:48:28 +0100

    simplify file formats to only include markdown, change readData result from object to array

--- test/test-petrify.js
+++ test/test-petrify.js
@@ -1,36 +1,12 @@
 var petrify = require('petrify');
 
-exports.testReadFileTxt = function(test){
-    test.expect(1);
-    var filename = __dirname + '/fixtures/data/file1.txt';
-    petrify.readFile(filename, function(err, data){
-        test.same(data, {
-            key1: 'value1',
-            key2: 'value2',
-            body: 'some text\nsome more text'
-        });
-        test.done();
-    });
-};
-
-exports.testReadFileHTML = function(test){
-    test.expect(1);
-    var filename = __dirname + '/fixtures/data/file2.html';
-    petrify.readFile(filename, function(err, data){
-        test.same(data, {
-            key1: 'value1',
-            key2: 'value2',
-            body: '<h1>Test</h1>'
-        });
-        test.done();
-    });
-};
 
 exports.testReadFileMarkdown = function(test){
     test.expect(1);
-    var filename = __dirname + '/fixtures/data/file3.md';
+    var filename = __dirname + '/fixtures/data/file1.md';
     petrify.readFile(filename, function(err, data){
         test.same(data, {
+            filename: 'file1.md',
             key1: 'value1',
             key2: 'value2',
             body: '<h1>Test</h1>\n\n<ul>\n<li>one</li>\n<li>two</li>\n</ul>'
@@ -42,23 +18,19 @@ exports.testReadFileMarkdown = function(test){
 exports.testReadData = function(test){
     test.expect(1);
     petrify.readData(__dirname + '/fixtures/data', function(err, data){
-        test.same(data, {
-            'file1.txt': {
-                key1:'value1',
-                key2:'value2',
-                body:'some text\nsome more text'
+        test.same(data, [
+            {
+                filename:'file2.md',
+                key:'value',
+                body:'<h1>Test 2</h1>'
             },
-            'file2.html': {
-                key1:'value1',
-                key2:'value2',
-                body:'<h1>Test</h1>'
-            },
-            'file3.md': {
+            {
+                filename: 'file1.md',
                 key1:'value1',
                 key2:'value2',
                 body:'<h1>Test</h1>\n\n<ul>\n<li>one</li>\n<li>two</li>\n</ul>'
             }
-        });
+        ]);
         test.done();
     });
 };
